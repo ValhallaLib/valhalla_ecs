39e38ce93193f9327adbbd9a31b5e16cc279af5c
João Lourenço <jlourenco5691@gmail.com>
2021-02-17 21:34:30 +0000

    examples: add query benchmark
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

---
+++ examples/benchmarks/query/source/app.d
@@ -0,0 +1,83 @@
+module app;
+
+import ecs.entity;
+import ecs.storage;
+import ecs.query;
+import ecs.queryfilter;
+
+import std.algorithm : each;
+import std.datetime.stopwatch : benchmark;
+import std.range : iota;
+import std.stdio;
+import std.typecons : Tuple;
+
+struct PositionComponent
+{
+	loat x = 0.0f;
+	loat y = 0.0f;
+}
+
+struct DirectionComponent
+{
+	float x = 0.0f;
+	float y = 0.0f;
+}
+
+struct ComflabulationComponent
+{
+	float thingy = 0.1f;
+	int dingy;
+	bool mingy;
+	string stringy = "";
+}
+
+void main()
+{
+	auto em = new EntityManager();
+	enum Loops = 100_000;
+
+	3_000.iota.each!(i => em.gen!(PositionComponent)());
+	3_000.iota.each!(i => em.gen!(PositionComponent, DirectionComponent)());
+	1_000.iota.each!(i => em.gen!(ComflabulationComponent, DirectionComponent)());
+	1_000.iota.each!(i => em.gen!(ComflabulationComponent, DirectionComponent, PositionComponent)());
+	2_000.iota.each!(i => em.gen!(DirectionComponent, ComflabulationComponent)());
+
+	benchmark!({
+		foreach (pos, dir; em.query!(Tuple!(PositionComponent, DirectionComponent)))
+		{
+			pos.x += dir.x * 0.15;
+			pos.y += dir.y * 0.15;
+		}
+	},{
+		foreach (com; em.query!(ComflabulationComponent))
+		{
+			com.thingy *= 1.0000001f;
+			com.dingy++;
+			com.mingy = !com.mingy;
+		}
+	},{
+		foreach (pos, dir, com; em.query!(Tuple!(PositionComponent, DirectionComponent, ComflabulationComponent)))
+		{
+			if ((com.mingy = !com.mingy) == false)
+			{
+				com.dingy++;
+
+				pos.x += dir.x * com.thingy;
+				pos.y += dir.y * com.thingy;
+			}
+
+			com.thingy *= 1.0000001f;
+		}
+	},{
+		foreach (pos, i; em.query!(Tuple!(PositionComponent, int)))
+		{
+			pos.x++;
+		}
+	},{
+		foreach (pos, dir; em.query!(Tuple!(PositionComponent, DirectionComponent), With!(ComflabulationComponent)))
+		{
+			pos.x += dir.x * 0.15;
+			pos.y += dir.y * 0.15;
+		}
+	})(Loops).each!(dur => writeln(dur/Loops));
+}
