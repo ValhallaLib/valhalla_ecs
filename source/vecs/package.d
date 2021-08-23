de5c1bb743ca0569b9ce14d26aad7bb2d7332e1b
João Lourenço <jlourenco5691@gmail.com>
2021-08-23 20:26:55 +0100

    vecs: remove Query implementation
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

--- source/vecs/package.d
+++ source/vecs/package.d
@@ -3,9 +3,6 @@ module vecs;
 public:
 	import vecs.entitymanager;
 	import vecs.entitybuilder;
-	import vecs.query;
-	import vecs.queryfilter;
-	import vecs.queryworld;
 	import vecs.signal;
 	import vecs.storage;
 	import vecs.resource;
