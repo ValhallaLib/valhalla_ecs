ae950f393b3e23b62876c17c5e8946428b0d5c7e
João Lourenço <jlourenco5691@gmail.com>
2021-08-24 18:45:05 +0100

    query: add Query helpers
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

--- source/vecs/query.d
+++ source/vecs/query.d
@@ -2,12 +2,16 @@ module vecs.query;
 
 import vecs.entity;
 import vecs.entitymanager;
+import vecs.storage;
 
 import std.format : format;
 import std.meta : All = allSatisfy;
+import std.meta : IndexOf = staticIndexOf;
+import std.meta : Map = staticMap;
 import std.meta : Not = templateNot;
-import std.meta : ApplyRight, Filter;
-import std.traits : hasUDA;
+import std.meta : AliasSeq, ApplyRight, Filter;
+import std.traits : hasUDA, TemplateArgsOf, TemplateOf;
+import std.typecons : Tuple, tuple;
 
 private enum QueryRule;
 
@@ -52,6 +56,63 @@ template Query(EntityManagerT, Select, Rules...)
 	static assert(is(Select == S!Args, alias S = .Select, Args...),
 		"Type (%s) must be a valid 'vecs.query.Select' type".format(Select.stringof)
 	);
+
+
+	/*
+	Get all indices of a Rule in Rules
+
+	given: AliasSeq!(With!(...), Without!(...), With!(...), With!(...))
+	yield: AliasSeq!(0, 2, 3)
+	*/
+	template RulePositions(alias Rule, size_t offset = 0)
+	{
+		enum i = IndexOf!(Rule, Map!(TemplateOf, Rules[offset .. $]));
+		static if (i >= 0)
+			alias RulePositions = AliasSeq!(i + offset, RulePositions!(Rule, i + offset + 1));
+		else
+			alias RulePositions = AliasSeq!();
+	}
+
+	/*
+	Get all offsets by Rule in Rule args
+
+	given: AliasSeq!(With!(int, uint), Without!(string), With!(ulong))
+	yield: AliasSeq!(tuple(0, 2), tuple(2, 3), tuple(3, 4))
+	*/
+	template RuleArgsOffsets(size_t index = 0, size_t from = 0)
+	{
+		alias Slice = Rules[index .. $];
+		static if (Slice.length)
+		{
+			enum to = from + TemplateArgsOf!(Slice[0]).length;
+			alias RuleArgsOffsets = AliasSeq!(tuple(from, to), RuleArgsOffsets!(index + 1, to));
+		}
+		else
+			alias RuleArgsOffsets = AliasSeq!();
+	}
+
+
+	alias Fun = TemplateArgsOf!EntityManagerT[0];
+	alias StorageOf(Component) = Storage!(Component, Fun);
+	alias ElementsAt(Tuple!(size_t, size_t) t, Seq...) = Seq[t[0] .. t[1]];
+	enum RuleArgsOffsetAt(size_t pos) = RuleArgsOffsets!()[pos];
+	alias RuleElements(alias Rule, Seq...) = Map!(ApplyRight!(ElementsAt, Seq), Map!(RuleArgsOffsetAt, RulePositions!Rule));
+
+	// extracted components
+	alias SelectArgs = TemplateArgsOf!Select;
+	alias RulesArgs = Map!(TemplateArgsOf, Rules);
+
+	// ctor types
+	alias SelectStorages = Map!(StorageOf, SelectArgs);
+	alias RuleStorages = Map!(StorageOf, RulesArgs);
+
+	// searching types
+	alias Include = AliasSeq!(SelectStorages, RuleElements!(With, RuleStorages));
+	alias Exclude = RuleElements!(Without, RuleStorages);
+
+	// component foreach output type
+	alias ElementType = Tuple!(Entity, Map!(PointerOf, SelectArgs));
+	alias PointerOf(T) = T*;
 
 
 	struct Query
